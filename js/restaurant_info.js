ddb4f88004fd21b56573ac9a79ad6ade4fa9c69a
Charles <charles.r.ribeiro@gmail.com>
2019-07-08 00:31:02 -0300

    Correção service worker

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiY2h1Y2ticiIsImEiOiJjanZlYzZmbnYwMGQ1M3lxbDhxZmgyaXhuIn0.6-2pQ_G_OntdurXcmcoaaw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
 console.log("sw");
+
+self.addEventListener('install', function(event){
+
+    console.log("install");
+})
